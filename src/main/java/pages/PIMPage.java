29edeca97b6fcb0c94835774022c6d6f4b809736
Steve <smuturi795@gmail.com>
2026-04-26 00:57:10 +0300

    Setup config management for URLs and credentials
    - Centralized URLs and login credentials
    - Removed hardcoded values from tests
    - Made configuration easier to update

---
+++ src/main/java/pages/PIMPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import com.microsoft.playwright.Locator;
+
+public class PIMPage {
+//    private final Locator txtFirstName;
+//    private final Locator txtMiddleName;
+//    private final Locator txtLastName;
+//    private final Locator strEmployeeId;
+//    private final Locator btnSave;
+}
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,18 +4,20 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.ConfigReader;
 
 public class BaseTest {
     protected Browser browser;
     protected Page page;
     BrowserFactory browserFactory;
+    ConfigReader configReader=ConfigReader.getInstance();
 
     @BeforeMethod
     public void setup(){
         browserFactory=new BrowserFactory();
         browser=browserFactory.createBrowser();
         page= browser.newPage();
-        page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        page.navigate(configReader.getProperty("OrangeHrm.url"));
     }
 
     @AfterMethod
